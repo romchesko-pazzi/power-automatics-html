a15f5be9d4d9c3a5eb839ce5a99b285224145a46
Roma Konstantyuk <romchesko.pazzi@gmail.com>
2025-02-17 09:33:29 +0300

    keks

--- js/yandexMap.js
+++ js/yandexMap.js
@@ -1,12 +1,12 @@
 ymaps.ready(init)
       function init() {
         const myMap = new ymaps.Map('map', {
-          center: [52.809399, 27.569637], // Координаты центра карты
+          center: [52.809473, 27.569865], // Координаты центра карты
           zoom: 16
         })
 
         const myPlacemark = new ymaps.Placemark(
-          [52.809399, 27.569637], // Координаты метки
+          [52.809473, 27.569865], // Координаты метки
           {},
           {
             preset: 'islands#blueDotIcon' // Синий кружок
